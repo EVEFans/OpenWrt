3e7c0539024b831dff19c84a6978675566a5dfef
EVEFans <86581414+EVEFans@users.noreply.github.com>
2022-05-19 21:41:34 +0800

    Update feeds_libray.sh

--- config/ImmortalWrt/feeds_libray.sh
+++ config/ImmortalWrt/feeds_libray.sh
@@ -17,9 +17,6 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
-sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
-
 # Kenzok8开发及收集常用OpenWrt软件包源码合集，通用版luci适合18.06与19.07
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
@@ -27,6 +24,3 @@ sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luc
 # Argon主题；Argon主题配置应用；vssr(Helloworld)上网冲浪应用；jd-dailybonus 京豆签到插件
 # go-aliyundrive-webdav阿里云盘webdav的golang实现；lua-maxminddb lua版本的maxminddb ip库解析器
 echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
-
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
