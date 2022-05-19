a87b15fd14ecf4b9901c28741663ddc924e76a52
EVEFans <86581414+EVEFans@users.noreply.github.com>
2022-05-19 21:42:12 +0800

    Update DIY_config.sh

--- config/ImmortalWrt/DIY_config.sh
+++ config/ImmortalWrt/DIY_config.sh
@@ -13,5 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.11/g' package/base-files/files/bin/config_generate
 
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
+# sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
+
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
