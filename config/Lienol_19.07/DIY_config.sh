690772b967025bcb406325760330c4401b16b8a4
EVEFans <86581414+EVEFans@users.noreply.github.com>
2022-05-20 01:04:44 +0800

    Update DIY_config.sh

--- config/Lienol_19.07/DIY_config.sh
+++ config/Lienol_19.07/DIY_config.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
