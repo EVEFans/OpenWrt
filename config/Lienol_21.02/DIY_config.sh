0ecef7000cde89383f9ff7b33c3a687a374bca9c
EVEFans <86581414+EVEFans@users.noreply.github.com>
2022-05-21 01:17:37 +0800

    Update DIY_config.sh

--- config/Lienol_21.02/DIY_config.sh
+++ config/Lienol_21.02/DIY_config.sh
@@ -16,5 +16,8 @@ sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generat
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 
-#OpenWrt的家长管理插件，支持游戏、视频、聊天、下载等app过滤
+# 家长管理插件，支持游戏、视频、聊天、下载等app过滤
 git clone https://github.com/destan19/OpenAppFilter.git package/oaf
+
+# 京东签到插件
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jd
