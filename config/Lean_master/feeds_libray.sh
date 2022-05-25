a27bccca208467a581fb48bed5daa390225fb821
EVEFans <86581414+EVEFans@users.noreply.github.com>
2022-05-25 20:27:49 +0800

    Update feeds_libray.sh

--- config/Lean_master/feeds_libray.sh
+++ config/Lean_master/feeds_libray.sh
@@ -14,7 +14,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Kenzok8开发及收集常用OpenWrt软件包源码合集，通用版luci适合18.06与19.07
