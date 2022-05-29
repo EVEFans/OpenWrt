51c61718e8de0bfb3fbbfe7a32708deaf49caae4
EVEFans <86581414+EVEFans@users.noreply.github.com>
2022-05-30 00:31:24 +0800

    Update DIY_config_aarch64.sh

--- config/Lean_master/DIY_config_aarch64.sh
+++ config/Lean_master/DIY_config_aarch64.sh
@@ -14,15 +14,16 @@
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
 #add
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/other/luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/other/Maxminddb
-git clone https://github.com/kenzok8/luci-theme-ifit.git package/other/luci-theme-ifit
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne package/other/luci-theme-argonne
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argonne-config package/other/luci-app-argonne-config
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/other/luci-app-adguardhome
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/other/luci-app-amlogic
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/other/luci-app-jd-dailybonus
 git clone https://github.com/destan19/OpenAppFilter.git package/other/luci-app-oaf
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-unblockneteasemusic package/other/luci-app-unblockneteasemusic
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/other/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/other/Maxminddb
+git clone https://github.com/sirpdboy/netspeedtest package/other/netspeedtest
+git clone https://github.com/kenzok8/luci-theme-ifit.git package/other/luci-theme-ifit
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne package/other/luci-theme-argonne
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argonne-config package/other/luci-app-argonne-config
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
